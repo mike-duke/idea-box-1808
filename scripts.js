f50c48d372b4faec526bf8742d7827484e5019bb
Matt Franzen <mattknowswhatsup@gmail.com>
2018-09-02 13:58:07 -0600

    Prepend to DOM working

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-var title = $('.input-title');
-console.log(title);
-
 $('.input-submit').on('click', function(){
   event.preventDefault()
   addCard()
@@ -8,10 +5,20 @@ $('.input-submit').on('click', function(){
 
 function Card (title, body) {
   this.title= title;
-  this.body=body;
+  this.body= body;
+  this.quality='swill'
 }
 
 function addCard() {
-  var card = new Card($('.input-title'), $('.input-body'))
-  console.log(card)
+  var card = new Card($('.input-title').val(), $('.input-body').val())
+  var newCard = `
+    <article class="new-card">
+      <h2>${card.title}</h2>
+      <button>Delete</button>
+      <p>${card.body}</p>
+      <button>Up</button>
+      <button>Down</button>
+      <h4>Quality: ${card.quality}</h4>`
+  $('.lists').prepend(newCard)
+
 }
