192318578265f3f7ad0e07272be59df5a776bbcf
Mike Duke <aithon@gmail.com>
2018-09-04 12:31:21 -0600

    Add comments to JS for clarity

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
-console.log(Object.keys(localStorage))
-
 getAllIdeas();
 
 $('.input-title').focus();
 
+// event listeners
 $('.lists').on('click', function(event) {
   deleteCard(event);
   upvote(event);
@@ -17,47 +16,20 @@ $('.input-submit').on('click', function(){
   clearInputs()
 })
 
+$('.lists').on('click', function(event) {
+  console.log($(event.target))
+  var card = getCard(event);
+})
+
+// constructor function
 function Card (title, body) {
   this.title = title;
   this.body = body;
   this.quality ='swill'
   this.storageId = Date.now();
 };
-
-function upvote(event) {
-  var retrievedCard = getCard(event);
-  var upvoteTarget = event.target.classList.contains('upvote-button');
-  if (upvoteTarget && retrievedCard.quality === 'swill') {
-    retrievedCard.quality = 'plausible';
-    var stringifiedCard = JSON.stringify(retrievedCard);
-    $(event.target).siblings('.card-quality').text('Quality: plausible');
-  } else if (upvoteTarget && retrievedCard.quality === 'plausible') {
-    retrievedCard.quality = 'genius';
-    var stringifiedCard = JSON.stringify(retrievedCard);
-    $(event.target).siblings('.card-quality').text('Quality: genius');
-  } else {
-    return; 
-  }
-  localStorage.setItem(retrievedCard.storageId, stringifiedCard);
-};
-
-function downvote(event) {
-  var retrievedCard = getCard(event);
-  var downvoteTarget = event.target.classList.contains('downvote-button');
-  if (downvoteTarget && retrievedCard.quality === 'genius') {
-    retrievedCard.quality = 'plausible';
-    var stringifiedCard = JSON.stringify(retrievedCard);
-    $(event.target).siblings('.card-quality').text('Quality: plausible');
-  } else if (downvoteTarget && retrievedCard.quality === 'plausible') {
-    retrievedCard.quality = 'swill';
-    var stringifiedCard = JSON.stringify(retrievedCard);
-    $(event.target).siblings('.card-quality').text('Quality: swill');
-  } else {
-    return; 
-  }
-  localStorage.setItem(retrievedCard.storageId, stringifiedCard);
-};
       
+// function to create card on DOM and store card in localStorage
 function addCard(card) {
   var stringifiedCard = JSON.stringify(card);
   var stringId = card.storageId.toString();
@@ -76,6 +48,7 @@ function addCard(card) {
   $('.lists').prepend(newCard)
 };
       
+// function to grab all of the ideas in localStorage
 function getAllIdeas() {
   var localArray = Object.keys(localStorage);
   for (i = 0; i < localArray.length; i++) {
@@ -83,12 +56,21 @@ function getAllIdeas() {
   }
 }
 
+// function to get the card from localStorage
+function getCard(event) {
+  var cardId = $(event.target).siblings('#storage-id').attr('class');
+  var retrievedCard = JSON.parse(localStorage.getItem(cardId));
+  return retrievedCard;
+}
+
+// function to clear inputs and ready the cursor
 function clearInputs() {
   $('.input-title').val('')
   $('.input-body').val('')
   $('.input-title').focus()
 };
 
+// function to run the delete button on the card
 function deleteCard(event) {
   var cardId = $(event.target).siblings('#storage-id').attr('class');
   if (event.target.classList.contains('delete-button')) {
@@ -97,8 +79,38 @@ function deleteCard(event) {
   }
 };
 
-function getCard(event) {
-  var cardId = $(event.target).siblings('#storage-id').attr('class');
-  var retrievedCard = JSON.parse(localStorage.getItem(cardId));
-  return retrievedCard;
-}
+// function to run the upvote button
+function upvote(event) {
+  var retrievedCard = getCard(event);
+  var upvoteTarget = event.target.classList.contains('upvote-button');
+  if (upvoteTarget && retrievedCard.quality === 'swill') {
+    retrievedCard.quality = 'plausible';
+    var stringifiedCard = JSON.stringify(retrievedCard);
+    $(event.target).siblings('.card-quality').text('Quality: plausible');
+  } else if (upvoteTarget && retrievedCard.quality === 'plausible') {
+    retrievedCard.quality = 'genius';
+    var stringifiedCard = JSON.stringify(retrievedCard);
+    $(event.target).siblings('.card-quality').text('Quality: genius');
+  } else {
+    return; 
+  }
+  localStorage.setItem(retrievedCard.storageId, stringifiedCard);
+};
+
+// function to run the downvote button
+function downvote(event) {
+  var retrievedCard = getCard(event);
+  var downvoteTarget = event.target.classList.contains('downvote-button');
+  if (downvoteTarget && retrievedCard.quality === 'genius') {
+    retrievedCard.quality = 'plausible';
+    var stringifiedCard = JSON.stringify(retrievedCard);
+    $(event.target).siblings('.card-quality').text('Quality: plausible');
+  } else if (downvoteTarget && retrievedCard.quality === 'plausible') {
+    retrievedCard.quality = 'swill';
+    var stringifiedCard = JSON.stringify(retrievedCard);
+    $(event.target).siblings('.card-quality').text('Quality: swill');
+  } else {
+    return; 
+  }
+  localStorage.setItem(retrievedCard.storageId, stringifiedCard);
+};
